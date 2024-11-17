17448c7ca16f5f03155085ea12f4aa00afe7c241
Hugo Casanave <casanave.hugo@gmail.com>
2024-11-17 13:02:30 +0100

    first page without styling

--- home/models.py
+++ home/models.py
@@ -5,9 +5,41 @@ from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 
 
+
 class HomePage(Page):
+    """Home page model."""
+
+    template = "home/home_page.html"
+    max_count = 1
+
+    banner_title = models.CharField(max_length=100, blank=True, null=True)
+    banner_subtitle = RichTextField(features=["bold","italic"], null=True)
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        FieldPanel("banner_image"),
         FieldPanel('body'),
+        FieldPanel("banner_cta")
+
     ]
+
+    class Meta: 
+        verbose_name= "Home Page"
+        verbose_name_plural = "Home Pages"
